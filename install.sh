affa6e70a314f085f18b8123cf5db9bdc72cbb00
TaoBeier <zhangjintao9020@gmail.com>
2018-01-15 14:09:27 +0800

    add --no-check-certificate close #19

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/tao12345666333/vim/master/vimrc -O $HOME/.vimrc
+wget --no-check-certificate https://raw.githubusercontent.com/tao12345666333/vim/master/vimrc -O $HOME/.vimrc
 
 vim -E -u $HOME/.vimrc +qall
 
